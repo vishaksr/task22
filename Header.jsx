c645c7106268bfef43007868e33bc1ea4f6f3839
vishaksr <166787536+vishaksr@users.noreply.github.com>
2024-07-31 19:23:37 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+
+function Header() {
+ 
+
+  return (
+    <div>
+        <div className='head1'>
+            <p>Pricing</p>
+        </div>
+    </div>
+  );
+}
+
+export default Header;
+
+ 
